1747ffb1bc7fa5592be5b4f7449adacd3bf1d87f
ajaynema <ajay.nema@gmail.com>
2025-02-02 13:12:32 +0530

    added tasks franework

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from spec_manager_agent import SpecManagerAgent
 from spec_provider import SpecProvider
 from config_provider import  ConfigProvider
+from spec_manager_task import SpecManagerTask
 
 SpecProvider("example/school").test()
 spec_manager_agent = SpecManagerAgent().test()
+spec_manager_task = SpecManagerTask().test()
 ConfigProvider().test()
